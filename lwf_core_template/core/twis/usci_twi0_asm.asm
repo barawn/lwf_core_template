776f840be32c061b90189e29c966260d2d9daafd
barawn <allison.122@osu.edu>
2015-08-12 08:18:42 -0400

    Fix first-byte handling in transmit (wait for UCTXSTT to clear). Also bracket ISRs with asmfunc for debugging.

--- lwf_core_template/core/twis/usci_twi0_asm.asm
+++ lwf_core_template/core/twis/usci_twi0_asm.asm
@@ -9,6 +9,7 @@ GlobalIf:	.if	USCI_TWI0_H_ = 1
 	.global	USCI_Twi0_FLAG_ISR
 	.global	USCI_Twi0_RXTX_ISR
 
+	.asmfunc
 USCI_Twi0_FLAG_ISR:
 	; One of the "bad things happened" flags went off.
 	bit.b	#UCNACKIFG, &usci_i2c_STAT
@@ -21,17 +22,28 @@ USCI_Twi0_FLAG_ISR_no_stop:
 	isr_lwevent_post_nocheck USCI_Twi0_state_lwevent
 	bic		#(LPM3), 0(SP)
 	reti
+	.endasmfunc
 
+
+	.asmfunc
 USCI_Twi0_RXTX_ISR:
 	bit.b	#usci_i2c_RXIFG, &usci_i2c_IFG
 	jnz		USCI_Twi0_RX_ISR
-	push	r15
-	mov.w	&USCI_Twi0_isr_data_ptr, r15
-	mov.w	@r15+, &usci_i2c_TXBUF
-	mov.w	r15, &USCI_Twi0_isr_data_ptr
-	pop		r15
+	bit.b	#UCTXSTT, &usci_i2c_CTL1
+	jnz		USCI_Twi0_TX_ISR_start_not_done
 	dec.b	&USCI_Twi0_isr_count
 	jz		USCI_Twi0_TX_ISR_done
+	push	r15
+	mov.w	&USCI_Twi0_isr_data_ptr, r15
+	mov.b	@r15+, &usci_i2c_TXBUF
+	mov.w	r15, &USCI_Twi0_isr_data_ptr
+	pop		r15
+	reti
+USCI_Twi0_TX_ISR_start_not_done:
+	push	r15
+	mov.w	&USCI_Twi0_isr_data_ptr, r15
+	mov.b	@r15, &usci_i2c_TXBUF
+	pop		r15
 	reti
 USCI_Twi0_TX_ISR_done:
 	bis.b	#UCTXSTP, &usci_i2c_CTL1
@@ -61,6 +73,7 @@ USCI_Twi0_RX_ISR_done:
 	isr_lwevent_post_nocheck USCI_Twi0_state_lwevent
 	bic		#(LPM3), 0(SP)
 	reti
+	.endasmfunc
 
 USCI_Twi0_If0:	.if	USCI_Twi0_RXTX_VECTOR_LOCATION != 0
 	.sect 	USCI_Twi0_RXTX_VECTOR_LOCATION
